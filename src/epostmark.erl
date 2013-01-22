74c16d9b803e058924ae6090e4c396d13398ba3c
Github Account <github0000@gmail.com>
2013-01-22 07:22:32 +0000

    Handle 422 response.

--- src/epostmark.erl
+++ src/epostmark.erl
@@ -2,14 +2,12 @@
 
 -export([send_email/1]).
 
--record(postmark_success, {obj}).
-
 authentication_header() ->
     {ok, PK} = application:get_env(epostmark, postmark_key),
     {<<"X-Postmark-Server-Token">>, PK}.
 
 send_email(Json) ->
-    Res = lhttpc:request(
+    Response = lhttpc:request(
         "https://api.postmarkapp.com/email",
         "POST",
         [
@@ -20,5 +18,29 @@ send_email(Json) ->
         Json,
         5000
     ),
-    {ok, {{200, _}, _, JsonResponse}} = Res,
-    {ok, #postmark_success{obj = jiffy:decode(JsonResponse)}}.
+    handle_response(Response).
+
+handle_response({ok, {{200, _}, _, JsonResponse}}) ->
+    {ok, jiffy:decode(JsonResponse)};
+
+handle_response({ok, {{422, "Unprocessable Entity"}, _, JsonResponse}}) ->
+    Jterm = jiffy:decode(JsonResponse),
+    case get_json_value([<<"ErrorCode">>], Jterm) of
+        300 ->
+            {error, {illegal_to_address, JsonResponse}};
+        _ ->
+            {error, JsonResponse}
+    end.
+
+get_json_value(_, undefined) ->
+    undefined;
+get_json_value([], Obj) ->
+    Obj;
+get_json_value([Key | Rest], {Obj}) ->
+    get_json_value(Rest, proplists:get_value(Key, Obj));
+get_json_value([Index | _Rest], []) when is_integer(Index) ->
+    undefined;
+get_json_value([Index | Rest], Obj) when is_integer(Index), is_list(Obj) ->
+    get_json_value(Rest, lists:nth(Index + 1, Obj));
+get_json_value([Key | Rest], Obj) when is_list(Obj) ->
+    get_json_value(Rest, proplists:get_all_values(Key, Obj)).
